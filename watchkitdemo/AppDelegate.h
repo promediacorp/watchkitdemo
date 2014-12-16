602b5d5a967de488a978c126b8f268d011bad248
Avi Wilensky <avi@promediacorp.com>
2014-12-16 16:11:42 -0500

    Initial Commit

---
+++ watchkitdemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  watchkitdemo
+//
+//  Created by Avi Wilensky on 12/16/14.
+//  Copyright (c) 2014 Avi Wilensky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ watchkitdemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  watchkitdemo
+//
+//  Created by Avi Wilensky on 12/16/14.
+//  Copyright (c) 2014 Avi Wilensky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
